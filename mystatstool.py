ad946cbcb6f5ea038b03d2b319b8f0954fbdefc6
OssieAckermann <ossie.ackermann@gmail.com>
2020-12-16 00:05:33 -0700

    data

---
+++ mystatstool.py
@@ -0,0 +1,17 @@
+
+class regression():
+    """
+    This is class contains all the 
+    statistical means I need for my 
+    pair trading project
+    """
+    def __init__(self, * args):
+        self.x = args[0]
+        self.y = args[1]
+
+    def fit(self):
+        return self.x
+
+    
+
+
